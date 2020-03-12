ea2de3a4ac6032e3d891113906229f8a89f8ad83
Edgar <edgarcovarel@yahoo.fr>
2020-03-12 17:21:56 +0100

    change

--- routes/index.js
+++ routes/index.js
@@ -429,7 +429,6 @@ router.post('/load-menu', async function(req, res, next) {
 
 });
 
-<<<<<<< HEAD
 // ROUTE POST POUR LA CONFIGURATION DE PRODUIT
 
 router.post('/new-produit', async function(req, res, next) {
@@ -482,7 +481,7 @@ router.post('/new-produit', async function(req, res, next) {
       if (restoBdd && right && position != null && priceWithTva != null) {
         console.log('priceWithTva :', priceWithTva);
         restoBdd.menu[position].products.push(
-          { name: req.body.produitName, price: priceWithTva, tva: parseInt(req.body.produitTVA) }
+          { name: req.body.produitName, price: priceWithTva, quantity: 0, tva: parseInt(req.body.produitTVA) }
         )
         console.log('restoBdd :', restoBdd.menu);
         saveUser = await restoBdd.save();
@@ -560,7 +559,6 @@ router.post('/delete-produit', async function(req, res, next) {
   res.json({ result, restoBdd, error })
 
 });
-=======
 
 
 /**
@@ -582,7 +580,6 @@ router.get('/orderPayed', async function(req, res, next) {
 
 
 
->>>>>>> 9934d026be6763e3907c8a7a63c67c3919e71248
 
 
 module.exports = router;
